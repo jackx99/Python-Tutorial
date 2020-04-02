305418d58fb5bba4bb95c9d5375db13146652fda
Sen Coder <sen.coder9901@gmail.com>
2020-04-02 16:50:56 +0800

    Create SimplePythonProgram

---
+++ SimplePythonProgram.py
@@ -0,0 +1,31 @@
+print("My first python program")
+print("19 + 4 = " + str(19 + 4)) # It displays the sum of two numbers and str method converts given object to string object.
+print("34 / 2 = " + str(34 / 2))
+print("10 * 3 = " + str(10 * 3))
+print("17 - 8 = " + str(17 - 8))
+print("4**2 = " + str(4**2)) # Exponentiation
+print("16 // 2 = " + str(16 // 2)) # Floor Division
+print("57 % 4 = " + str(57 % 4)) # Modulo operator
+print("""Basic Elements of Python
+Mohsen Coder""") # Newlines
+# Assignment Statement
+length = 10
+width = 5
+rect = length * width
+print("area of rectangle is " + str(rect))
+GOLDEN_RATIO = 1.618 # Constant
+age = input("Enter your age: ") # Input (Read) Statement
+print("My Age is " + age)
+score = 1
+score += 5 # Compound Assignment Operator
+print("score " + str(score))
+# Escape Sequences
+print("Mohsen\nCoder") # The newline escape sequence
+print("Google\tApple") # The tab escape sequence
+print("      A")
+print("    \bB") # The backspace escape sequence
+print("    \rC") # The return escape sequence
+print("\\") # The backslash escape sequence
+print("\'Orange\'") # The single quotation escape sequence
+print("\"Orange\"") # The double quotation escape sequence
+print(r"'Mohsen Coder'") # Escape Sequences in Raw String
